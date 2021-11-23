9dcd4edde1faf14588b614538f9ad411a99f2782
Damian Urbaniak <damian.urbaniak@natzka.com>
2021-11-23 17:08:16 +0100

    rate cannot be float, adjusting also hpa

--- k6/load-test.js
+++ k6/load-test.js
@@ -1,12 +1,11 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
 
 export const options = {
     scenarios: {
         constant_rate_low: {
             executor: 'constant-arrival-rate',
-            rate: 0.1,
-            timeUnit: '1s',
+            rate: 1,
+            timeUnit: '10s',
             duration: '5m',
             preAllocatedVUs: 100,
         },
@@ -21,21 +20,21 @@ export const options = {
         },
 
         ramping_rate: {
-            executor: 'ramping-arrival-rate',  
-            startTime: '15m',    
-            startRate: 0,      
-            timeUnit: '1s',      
-            preAllocatedVUs: 100,      
-            maxVUs: 100,      
-            stages: [      
-              { target: 10, duration: '10m' },         
+            executor: 'ramping-arrival-rate',
+            startTime: '15m',
+            startRate: 0,
+            timeUnit: '1s',
+            preAllocatedVUs: 100,
+            maxVUs: 100,
+            stages: [
+              { target: 10, duration: '10m' },
             ],
-      
+
           },
     },
 };
 
 export default function () {
-    var url = 'https://predscaling01.natzkalabs.com/loads/matrixinverse?msize=1000';     
+    var url = 'https://predscaling01.natzkalabs.com/loads/matrixinverse?msize=500';     
     http.get(url);
 }
